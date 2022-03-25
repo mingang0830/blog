83362188db87d758a4eab5103772ac27775b490f
mingyeong <mingang0830@gmail.com>
2022-03-25 15:27:08 +0900

    board
    LoginForm 생성

--- board/forms.py
+++ board/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Board
 
+
 class WritePost(forms.Form):
     title = forms.CharField(max_length=512, required=True)
     content = forms.CharField(widget=forms.Textarea, required=True)
@@ -21,3 +22,9 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "password1", "password2", "email")
+
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
