cab27425fdd13e0003f5b78b73da2f5f7c1cceb2
mingyeong <mingang0830@gmail.com>
2022-04-24 11:50:37 +0900

    url 수정
    class based view 에 맞게 path  수정
    comment path에서 id 는 board id 이므로 'board/<int:id>/new_comment/' 로 수정

--- blog/urls.py
+++ blog/urls.py
@@ -16,17 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from board.views import board_list, detail, remove_post, write, edit_post, signup, signin, signout, new_comment
+from board.views import board_list, DetailView, write, edit_post, signup, signin, signout, comment
 
 urlpatterns = [
+    # path('', board_list),
     path('admin/', admin.site.urls),
     path('board/', board_list),
-    path('board/<int:id>', detail),
+    path('board/<int:id>', DetailView.as_view()),
     path('board/write', write),
-    path('board/<int:id>/remove', remove_post),
     path('board/<int:id>/edit', edit_post),
     path('board/signup', signup),
     path('board/signin', signin),
     path('board/logout', signout),
-    path('board/new_comment/<int:id>', new_comment)
+    path('board/<int:id>/new_comment/', comment)
 ]
