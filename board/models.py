e1e6854089b2c67b30e63681d03bd3af1d450e7f
mingyeong <mingang0830@gmail.com>
2022-07-13 22:07:37 +0900

    일단 익명으로 게시판 글쓰기 가능
    익명은 AnonymousUser 로 고정되어있음

--- board/models.py
+++ board/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Board(models.Model):
     title = models.CharField(max_length=512)
     content = models.TextField()
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null= True)
+    created_by = models.CharField(max_length=218)
     created_at = models.DateTimeField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
